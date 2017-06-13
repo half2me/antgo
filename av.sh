1535c9feb9af08f29755ddd27d511637069f8b56
Benjamin Tamási <h@lfto.me>
2017-06-14 00:05:39 +0200

    Appveyor updates

--- av.sh
+++ av.sh
@@ -7,12 +7,9 @@ export GOPATH=/go && \
 export CGO_ENABLED=1 && \
 pacman --noconfirm -S \
     mingw64/mingw-w64-x86_64-go \
-    mingw64/mingw-w64-x86_64-gcc \
-    mingw64/mingw-w64-x86_64-pkg-config \
-    mingw64/mingw-w64-x86_64-libusb \
-    msys/git && \
+    mingw64/mingw-w64-x86_64-libusb && \
 go version
 go get github.com/half2me/antgo/... && \
-go build -o bin/antgo-win64.exe -i github.com/half2me/antgo && \
-ls -la bin/
-echo "ALL DONE!"
+go build -o ~/antgo-win64.exe -i github.com/half2me/antgo && \
+cp /
+echo "Build complete!"
