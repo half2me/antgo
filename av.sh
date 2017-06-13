cc73b3ed997d82b75f97cd19d85e3a26a97028be
Tamási Benjamin <half2me@users.noreply.github.com>
2017-06-14 01:43:47 +0200

    Appveyor support (#3)

---
+++ av.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+export PATH="/mingw64/bin:${PATH}" && \
+export PKG_CONFIG_PATH="/mingw64/lib/pkgconfig:${PKG_CONFIG_PATH}" && \
+export GOROOT=/mingw64/lib/go && \
+export GOPATH=/go && \
+export CGO_ENABLED=1 && \
+pacman --noconfirm -S \
+    mingw64/mingw-w64-x86_64-go \
+    mingw64/mingw-w64-x86_64-libusb && \
+go version
+go get github.com/half2me/antgo/... && \
+go build -o antgo-win64.exe -i github.com/half2me/antgo && \
+echo "Build complete!" #
