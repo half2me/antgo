efb93c69c6a76338907d299ec6fb2dd6d6a14a04
Benjamin Tamási <h@lfto.me>
2017-06-13 23:56:57 +0200

    Appveyor updates

--- av.sh
+++ av.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export PATH="${PATH}:/mingw64/bin" && \
-export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/mingw64/lib/pkgconfig" && \
+export PATH="/mingw64/bin:${PATH}" && \
+export PKG_CONFIG_PATH="/mingw64/lib/pkgconfig:${PKG_CONFIG_PATH}" && \
 export GOROOT=/mingw64/lib/go && \
 export GOPATH=/go && \
 export CGO_ENABLED=1 && \
