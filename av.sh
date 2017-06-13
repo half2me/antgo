83190e15e1a89d3733aa01b0e4f89ecb790d0edd
Benjamin Tamási <h@lfto.me>
2017-06-13 22:04:37 +0200

    Appveyor updates

--- av.sh
+++ av.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-echo "yolo"
+export PATH="${PATH}:/mingw64/bin" && \
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/mingw64/lib/pkgconfig" && \
+export GOROOT=/mingw64/lib/go && \
+export GOPATH=/go && \
+export CGO_ENABLED=1 && \
+export GOARCH=amd64 && \
+pacman --noconfirm -S \
+    mingw64/mingw-w64-x86_64-go \
+    mingw64/mingw-w64-x86_64-gcc \
+    mingw64/mingw-w64-x86_64-pkg-config \
+    mingw64/mingw-w64-x86_64-libusb \
+    msys/git && \
+go get github.com/half2me/antgo/... && \
+go build -o ~/antgo-win64.exe -i github.com/half2me/antgo
