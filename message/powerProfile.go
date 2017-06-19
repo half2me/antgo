1b2172d1ff9000dca6a6f402e420c5e5a37c22b8
Benjamin Tamási <h@lfto.me>
2017-06-19 10:52:40 +0200

    SpeedAndCadenceProfile

--- message/powerProfile.go
+++ message/powerProfile.go
@@ -9,59 +9,59 @@ type PowerMessage AntBroadcastMessage
 
 // The specifies the type of message sent by the power sensor
 // Currently we only decode the standard Power-Only main data page (0x10)
-func (p PowerMessage) DataPageNumber() uint8 {
-	return uint8(AntBroadcastMessage(p).Content()[0])
+func (m PowerMessage) DataPageNumber() uint8 {
+	return uint8(AntBroadcastMessage(m).Content()[0])
 }
 
 // The update event count field is incremented each time the information in the message is updated.
 // There are no invalid values for update event count.
-func (p PowerMessage) EventCount() uint8 {
-	return uint8(AntBroadcastMessage(p).Content()[1])
+func (m PowerMessage) EventCount() uint8 {
+	return uint8(AntBroadcastMessage(m).Content()[1])
 }
 
 // Using the previous message we can see the difference of the event counts. Since sensors increment
 // this value by 1 every time they generate an ANT+ message, we can use this value to get an idea of
 // how many frames were dropped since the last message.
-func (p PowerMessage) EventCountDiff(prev PowerMessage) uint8 {
-	return p.EventCount() - prev.EventCount()
+func (m PowerMessage) EventCountDiff(prev PowerMessage) uint8 {
+	return m.EventCount() - prev.EventCount()
 }
 
 // The instantaneous cadence field is used to transmit the pedaling cadence recorded from the power sensor.
 // This field is an instantaneous value only; it does not accumulate between messages. (RPM)
-func (p PowerMessage) InstantaneousCadence() uint8 {
-	return uint8(AntBroadcastMessage(p).Content()[3])
+func (m PowerMessage) InstantaneousCadence() uint8 {
+	return uint8(AntBroadcastMessage(m).Content()[3])
 }
 
 // Accumulated power is the running sum of the instantaneous power data and is incremented at each update
 // of the update event count. (W)
-func (p PowerMessage) AccumulatedPower() (num uint16) {
-	binary.Read(bytes.NewReader(AntBroadcastMessage(p).Content()[4:6]), binary.LittleEndian, &num)
+func (m PowerMessage) AccumulatedPower() (num uint16) {
+	binary.Read(bytes.NewReader(AntBroadcastMessage(m).Content()[4:6]), binary.LittleEndian, &num)
 	return
 }
 
 // Using the previous message, get the calculated Power from the difference of the accumulated values.
 // This gives a more precise measurement and should be used instead of the instantaneous values. (W)
-func (p PowerMessage) AccumulatedPowerDiff(prev PowerMessage) (uint16) {
-	return p.AccumulatedPower() - prev.AccumulatedPower()
+func (m PowerMessage) AccumulatedPowerDiff(prev PowerMessage) (uint16) {
+	return m.AccumulatedPower() - prev.AccumulatedPower()
 }
 
 // Instantaneous power measurement (W)
-func (p PowerMessage) InstantaneousPower() (num uint16) {
-	binary.Read(bytes.NewReader(AntBroadcastMessage(p).Content()[6:8]), binary.LittleEndian, &num)
+func (m PowerMessage) InstantaneousPower() (num uint16) {
+	binary.Read(bytes.NewReader(AntBroadcastMessage(m).Content()[6:8]), binary.LittleEndian, &num)
 	return
 }
 
 // Under normal conditions with complete RF reception, average power equals instantaneous power.
 // In conditions where packets are lost, average power accurately calculates power over the interval
-// between the received messages.
-func (p PowerMessage) AveragePower(prev PowerMessage) uint16 {
+// between the received messages. (W)
+func (m PowerMessage) AveragePower(prev PowerMessage) float32 {
 	if prev == nil {
-		return p.InstantaneousPower()
+		return float32(m.InstantaneousPower())
 	}
 
-	if prev.EventCount() == p.EventCount() {
-		return p.InstantaneousPower()
+	if prev.EventCount() == m.EventCount() {
+		return float32(m.InstantaneousPower())
 	}
 
-	return p.AccumulatedPowerDiff(prev) / uint16(p.EventCountDiff(prev))
+	return float32(m.AccumulatedPowerDiff(prev)) / float32(m.EventCountDiff(prev))
 }
--- message/speedAndCadenceProfile.go
+++ message/speedAndCadenceProfile.go
@@ -1,3 +1,88 @@
 package message
 
-type speedAndCadenceMessage AntBroadcastMessage
+import (
+	"encoding/binary"
+	"bytes"
+)
+
+type SpeedAndCadenceMessage AntBroadcastMessage
+
+// Represents the time of the last valid bike cadence event (1/1024 sec)
+func (m SpeedAndCadenceMessage) CadenceEventTime() (num uint16) {
+	binary.Read(bytes.NewReader(AntBroadcastMessage(m).Content()[0:2]), binary.LittleEndian, &num)
+	return
+}
+
+// Represents the total number of pedal revolutions
+func (m SpeedAndCadenceMessage) CumulativeCadenceRevolutionCount() (num uint16) {
+	binary.Read(bytes.NewReader(AntBroadcastMessage(m).Content()[2:4]), binary.LittleEndian, &num)
+	return
+}
+
+// Represents the time of the last valid bike speed event (1/1024 sec)
+func (m SpeedAndCadenceMessage) SpeedEventTime() (num uint16) {
+	binary.Read(bytes.NewReader(AntBroadcastMessage(m).Content()[4:6]), binary.LittleEndian, &num)
+	return
+}
+
+// Represents the total number of wheel revolutions
+func (m SpeedAndCadenceMessage) CumulativeSpeedRevolutionCount() (num uint16) {
+	binary.Read(bytes.NewReader(AntBroadcastMessage(m).Content()[6:8]), binary.LittleEndian, &num)
+	return
+}
+
+func (m SpeedAndCadenceMessage) SpeedEventTimeDiff(prev SpeedAndCadenceMessage) uint16 {
+	return m.SpeedEventTime() - prev.SpeedEventTime()
+}
+
+func (m SpeedAndCadenceMessage) CadenceEventTimeDiff(prev SpeedAndCadenceMessage) uint16 {
+	return m.CadenceEventTime() - prev.CadenceEventTime()
+}
+
+func (m SpeedAndCadenceMessage) SpeedRevolutionCountDiff(prev SpeedAndCadenceMessage) uint16 {
+	return m.CumulativeSpeedRevolutionCount() - prev.CumulativeSpeedRevolutionCount()
+}
+
+func (m SpeedAndCadenceMessage) CadenceRevolutionCountDiff(prev SpeedAndCadenceMessage) uint16 {
+	return m.CumulativeCadenceRevolutionCount() - prev.CumulativeCadenceRevolutionCount()
+}
+
+// Returns the cadence calculated from the previous message (0 if previous message is nil)
+// The second parameter "nochange" indicates that the EventTime counter has not changed since the
+// previous message, which means a complete rotation has not yet occurred. You should use this value
+// to handle cases where the pedal stops: "coasting" (EventTime counter does not change)
+// Cadence: (RPM)
+func (m SpeedAndCadenceMessage) Cadence(prev SpeedAndCadenceMessage) (cadence float32, nochange bool) {
+	if prev == nil {
+		return 0, false
+	}
+
+	if m.CadenceEventTimeDiff(prev) == 0 {
+		return 0, true
+	}
+
+	return float32(m.CadenceRevolutionCountDiff(prev)) * 1024 * 60 / float32(m.CadenceEventTimeDiff(prev)), false
+}
+
+// Distance travelled since the last message: (m)
+// circumference: Circumference of the wheel (m)
+func (m SpeedAndCadenceMessage) Distance(prev SpeedAndCadenceMessage, circumference float32) float32 {
+	return float32(m.SpeedRevolutionCountDiff(prev)) * circumference
+}
+
+// Speed: (m/s)
+// circumference: Circumference of the wheel (m)
+// The second parameter "nochange" indicates that the EventTime counter has not changed since the
+// previous message, which means a complete rotation has not yet occurred. You should use this value
+// to handle cases where the pedal stops: "coasting" (EventTime counter does not change)
+func (m SpeedAndCadenceMessage) Speed(prev SpeedAndCadenceMessage, circumference float32) (speed float32, nochange bool) {
+	if prev == nil {
+		return 0, false
+	}
+
+	if m.SpeedEventTimeDiff(prev) == 0 {
+		return 0, true
+	}
+
+	return m.Distance(prev, circumference) / float32(m.SpeedEventTimeDiff(prev)), false
+}
